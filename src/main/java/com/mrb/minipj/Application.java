c314ec9af6ad19e6d1b142f101c980f55c1fbe71
MRB <bairongming@gzmpc.com>
2019-03-25 10:06:19 +0800

    添加计时日志

--- src/main/java/com/mrb/minipj/Application.java
+++ src/main/java/com/mrb/minipj/Application.java
@@ -3,8 +3,13 @@ package com.mrb.minipj;
 
 import com.google.gson.Gson;
 import com.mrb.minipj.repository.UserRepository;
+import com.mrb.minipj.utils.DateUtils;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,7 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
 import javax.sql.DataSource;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.*;
 
@@ -94,6 +104,7 @@ public class Application {
             return String.format("成功添加条数:%d,花费时间：%d 毫秒,其中最大耗时是：%d 毫秒",count,(end-start),maxCost);
         }
 
+        @CostLog
         @RequestMapping(value = "user/save")
         public String saveUser(String userName){
             try {
@@ -186,7 +197,7 @@ public class Application {
             user = userRepository.save(user);
             long end = System.currentTimeMillis();
             long cost = end-start;
-            log.info("save user:{} cost time {}",user.getId(),cost);
+            //log.info("{} save user:{} cost time {}",Thread.currentThread().getName(),user.getId(),cost);
             return cost;
         }
 
@@ -205,6 +216,27 @@ public class Application {
 
     }
 
+    @Slf4j
+    @Aspect
+    @Component
+    public static class ControllerAspect {
+        @Pointcut(value = " @annotation(com.mrb.minipj.Application.CostLog)")
+        public void addCostPoint(){}
 
+        @Around("addCostPoint()")
+        public Object doLog(ProceedingJoinPoint jp) throws Throwable {
+            long start = System.currentTimeMillis();
+            log.info("=====>{} receiced the request,now time : {} ",Thread.currentThread().getName(), DateUtils.format(new Date(start)));
+            Object result = jp.proceed();
+            long end = System.currentTimeMillis();
+            log.info("======>{} return the request,cost time :{}",Thread.currentThread().getName(),(end-start));
+            return result;
+        }
+    }
+
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    public static @interface CostLog {
+    }
 
 }
