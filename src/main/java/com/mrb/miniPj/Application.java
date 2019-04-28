e2d063fee0efd90c4c370113a3f53c6592889b7f
18312106568 <315077558@qq.com>
2019-04-28 23:53:30 +0800

    修正miniPj结构

---
+++ src/main/java/com/mrb/miniPj/Application.java
@@ -0,0 +1,36 @@
+package com.mrb.miniPj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MRB
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String args[]){
+        SpringApplication.run(Application.class, args);
+    }
+    
+    
+    @RestController
+    static class DefaultController{
+        
+        @RequestMapping
+        public String saveUserGood(Long id){
+            return "hello world";
+        }
+
+        
+    }
+
+}
