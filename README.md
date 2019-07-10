bf9fa543af1434363dc9cee680827c34fdf39a1e
MRB <bairongming@gzmpc.com>
2019-07-10 11:46:05 +0800

    mini kafka

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 **[miniPJ)
 极简风格的项目，以最小依赖最少文件来构建项目
 快速学习springboot
+* 下载[zookeeper](http://zookeeper.apache.org/releases.html#download) 
+* 下载[kafka](http://kafka.apache.org/downloads)
 
-分支结构
-* mini-es 极简嵌入elasticsearch
-* ex-mini-es es拓展应用
-* mini-redis 极简嵌入redis
-* mini-jpa 极简嵌入mysql
-* mini-freemarker 极简嵌入freemarker
+
--- src/main/java/com/mrb/miniPj/Application.java
+++ src/main/java/com/mrb/miniPj/Application.java
@@ -1,9 +1,20 @@
 package com.mrb.miniPj;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,17 +31,38 @@ public class Application {
     public static void main(String args[]){
         SpringApplication.run(Application.class, args);
     }
-    
-    
+
+    public static final String TOPIC = "KAFKA_LOG";
+
+
     @RestController
     static class DefaultController{
-        
-        @RequestMapping
-        public String saveUserGood(Long id){
-            return "hello world";
-        }
 
-        
+        @Autowired
+        private KafkaTemplate kafkaTemplate;
+
+        @RequestMapping("send-log")
+        public String sendLog(@RequestParam("value") String value) throws ExecutionException, InterruptedException {
+            ListenableFuture future = kafkaTemplate.send(TOPIC,value);
+            return  future.get().toString();
+        }
+    }
+
+
+    @Component
+    @Slf4j
+    public  static class LogConsumer {
+        @KafkaListener(topics = {TOPIC})
+        public void consumer(ConsumerRecord<?, ?> consumerRecord) {
+            //判断是否为null
+            Optional<?> kafkaMessage = Optional.ofNullable(consumerRecord.value());
+            log.info(">>>>>>>>>> record =" + kafkaMessage);
+            if (kafkaMessage.isPresent()) {
+                //得到Optional实例中的值
+                Object message = kafkaMessage.get();
+                System.err.println("消费消息:" + message);
+            }
+        }
     }
 
 }
