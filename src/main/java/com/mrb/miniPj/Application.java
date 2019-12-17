75655fcf9556beb78955d1752c2080cc77f6b299
MRB <bairongming@gzmpc.com>
2019-12-17 15:24:32 +0800

    mini-docker

--- src/main/java/com/mrb/miniPj/Application.java
+++ src/main/java/com/mrb/miniPj/Application.java
@@ -20,17 +20,17 @@ public class Application {
     public static void main(String args[]){
         SpringApplication.run(Application.class, args);
     }
-    
-    
+
+
     @RestController
     static class DefaultController{
-        
+
         @RequestMapping
         public String saveUserGood(Long id){
             return "hello world";
         }
 
-        
+
     }
 
 }
