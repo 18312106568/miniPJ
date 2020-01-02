19107a64c0aaf1d4c095bf7b5e6febdb9783fd1f
MRB <bairongming@gzmpc.com>
2020-01-02 09:55:02 +0800

    极简文件上传

--- src/main/java/com/mrb/miniPj/Application.java
+++ src/main/java/com/mrb/miniPj/Application.java
@@ -3,7 +3,14 @@ package com.mrb.miniPj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,17 +27,25 @@ public class Application {
     public static void main(String args[]){
         SpringApplication.run(Application.class, args);
     }
-    
-    
+
+
     @RestController
     static class DefaultController{
-        
-        @RequestMapping
-        public String saveUserGood(Long id){
-            return "hello world";
+
+        @RequestMapping(value = "upload",method = RequestMethod.POST)
+        @ResponseBody
+        public String upload(HttpServletRequest request) throws IOException {
+            InputStream inputStream = request.getInputStream();
+            Scanner scanner = new Scanner(inputStream);
+            StringBuilder sb = new StringBuilder();
+            while(scanner.hasNextLine()){
+                sb.append(scanner.nextLine()).append("\r\n");
+            }
+            System.out.println(sb.toString());
+            return sb.toString();
         }
 
-        
+
     }
 
 }
